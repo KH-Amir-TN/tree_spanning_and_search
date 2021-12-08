f7895221823ebe33151eeadd317757dd5c3d66a3
kh-amir-tn <amir.khemissi@insat.u-carthage.tn>
2021-12-08 22:27:10 +0100

    Enabling logging.

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+import time
+
+LOG_OUT = "./logs/"
+LOG_FILENAME = str( time.time() ) + ".log"
+LOG_AND_PRINT = False 
+
+LOG_FILE = open(LOG_OUT + LOG_FILENAME , "w")
+
+def log(msg,explicit_log_and_print=False):
+    if LOG_FILE:
+        LOG_FILE.writelines(str( msg ) + "\n")
+    if LOG_AND_PRINT or explicit_log_and_print :
+        print(msg)
 
 
-print("Hello world!")
+log("Test")
+
+LOG_FILE.close()
